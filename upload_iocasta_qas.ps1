a560c8c7a9dd23cea7363602a3291d90bc5ae86c
Francisco Ribeiro <francisco.ribeiro@aecom.com>
2026-05-26 16:11:16 -0300

    Ajustes

--- upload_iocasta_qas.ps1
+++ upload_iocasta_qas.ps1
@@ -1,5 +1,5 @@
 param(
-  [string]$Folder = "C:\Users\RibeiroF\Downloads\Nova pasta",
+  [string]$Folder = "L:\Secure_DCS\BRBLH1PINFW001\COE_Digital\coe_digital_data\silver_data\restricted\loc\localidades\IBGE\20251119\00",
   [string]$GeoServer = "https://gisqas.iocasta.com.br/geoserver",
   [string]$Catalog = "https://catalogqas.iocasta.com.br",
   [string]$Workspace = "gold",
