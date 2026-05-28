415284452488337ab88ddefd6027675844f3d308
Francisco Ribeiro <francisco.ribeiro@aecom.com>
2026-05-28 11:56:23 -0300

    Ajustes

--- upload_iocasta_qas.ps1
+++ upload_iocasta_qas.ps1
@@ -1,5 +1,5 @@
 param(
-  [string]$Folder = "L:\Secure_DCS\BRBLH1PINFW001\COE_Digital\coe_digital_data\silver_data\restricted\pcd\autos_infracao\IBAMA\20210915\00",
+  [string]$Folder = "L:\Secure_DCS\BRBLH1PINFW001\COE_Digital\coe_digital_data\silver_data\restricted\loc\setor_censitario\IBGE\20241114\00",
   [string]$GeoServer = "https://gisqas.iocasta.com.br/geoserver",
   [string]$Catalog = "https://catalogqas.iocasta.com.br",
   [string]$Workspace = "gold",
